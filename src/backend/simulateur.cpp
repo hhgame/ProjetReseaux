5daf301ad54681e66501bf2815c48b602d33c416
hhgame <76120042+hhgame@users.noreply.github.com>
2025-11-03 10:31:24 +0100

    Commit pour deploiement

--- src/backend/simulateur.cpp
+++ src/backend/simulateur.cpp
@@ -7,7 +7,7 @@
 #define RAYON_MAX 200
 
 Simulateur::Simulateur() {
-    const std::string pathMap = "../ProjetReseaux/map";
+    const std::string pathMap = "../../map";
     routes.chargerDepuisOSM(pathMap);
     routes.afficherResume();
     ajouterVehiculesAleatoires(2.0, 10.0);
